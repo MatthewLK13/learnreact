1e3755ce3196eb3c680bfc34bdffde371f11faae
khoi <ghepus7@gmail.com>
2024-03-25 08:31:27 +0700

    create some basic function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button>  
+      I'll make button
+    </button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
     </div>
   );
 }
 
-export default App;
+export default function MyApp() {
+  return(
+    <> 
+      <h1>  
+       I'll make button
+      </h1>
+      <MyButton />
+      <AboutPage />
+    </>
+  );
+}
